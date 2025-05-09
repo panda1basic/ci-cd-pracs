ae2e201a4477321916707b1a48e58caede46781c
egor <godmode000001@mail.ru>
2025-05-10 02:09:53 +0300

    fix

--- src/pr3/app.py
+++ src/pr3/app.py
@@ -1,25 +1,41 @@
 from flask import Flask, Response
-import logging
 from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
-REQUEST_COUNT = Counter('flask_app_requests_total', 'Total HTTP requests')
+REQUESTS = Counter(
+    'flask_app_requests_total',
+    'Общее число HTTP-запросов к Flask-приложению',
+    ['method', 'endpoint', 'http_status']
+)
 
 @app.before_request
-def count_requests():
-    REQUEST_COUNT.inc()
+def before_request():
+
+
+    pass
+
+@app.after_request
+def after_request(response):
+    REQUESTS.labels(
+        method  = flask.request.method,
+        endpoint= flask.request.path,
+        http_status=response.status_code
+    ).inc()
+    return response
 
 @app.route('/')
 def home():
-    logger.info('Home page requested')
+    app.logger.info('Received request on home page')
     return 'Hello, Monitoring!'
 
 @app.route('/metrics')
 def metrics():
-    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+    """
+    Экспозиция всех метрик Prometheus на /metrics
+    """
+    data = generate_latest()
+    return Response(data, mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
