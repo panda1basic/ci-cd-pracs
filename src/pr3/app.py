a0672ed10f4b6d600342752bbf9e28c4da0d7964
egor <godmode000001@mail.ru>
2025-05-10 01:12:44 +0300

    upd

--- src/pr3/app.py
+++ src/pr3/app.py
@@ -3,18 +3,19 @@ import logging
 from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-REQUEST_COUNT = Counter('flask_app_requests_total', 'Total HTTP requests to Flask app')
+REQUEST_COUNT = Counter('flask_app_requests_total', 'Total HTTP requests')
 
 @app.before_request
-def before_request():
+def count_requests():
     REQUEST_COUNT.inc()
 
 @app.route('/')
 def home():
-    logger.info('Received request on home page')
+    logger.info('Home page requested')
     return 'Hello, Monitoring!'
 
 @app.route('/metrics')
