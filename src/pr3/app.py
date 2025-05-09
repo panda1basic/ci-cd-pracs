86dd03465a80222d7866dcadb4056c702fa3baff
egor <godmode000001@mail.ru>
2025-05-10 02:30:11 +0300

    fix

--- src/pr3/app.py
+++ src/pr3/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
@@ -9,33 +9,22 @@ REQUESTS = Counter(
     ['method', 'endpoint', 'http_status']
 )
 
-@app.before_request
-def before_request():
-
-
-    pass
-
 @app.after_request
 def after_request(response):
     REQUESTS.labels(
-        method  = flask.request.method,
-        endpoint= flask.request.path,
+        method=request.method,
+        endpoint=request.path,
         http_status=response.status_code
     ).inc()
     return response
 
 @app.route('/')
 def home():
-    app.logger.info('Received request on home page')
     return 'Hello, Monitoring!'
 
 @app.route('/metrics')
 def metrics():
-    """
-    Экспозиция всех метрик Prometheus на /metrics
-    """
-    data = generate_latest()
-    return Response(data, mimetype=CONTENT_TYPE_LATEST)
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
